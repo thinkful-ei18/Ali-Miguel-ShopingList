5646243824760ccc082d0ddba163df8658cfb9bc
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-01-24 08:50:14 -0800

    Troubleshoot question 4

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,9 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
-});
-
-store.items.push(Item.create('apples'));
-
-api.createItem('pears', (newItem) => {
   api.getItems((items) => {
-    console.log(items);
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
   });
 });
+
