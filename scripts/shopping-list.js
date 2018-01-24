8f5f514fa89186fa87dc94e05473216c9adfdd48
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-01-24 11:06:13 -0800

    Step 8

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,3 +1,4 @@
+'use strict';
 /* global store api */
 
 // eslint-disable-next-line no-unused-vars
@@ -80,8 +81,11 @@ const shoppingList = (function(){
   function handleItemCheckClicked() {
     $('.js-shopping-list').on('click', '.js-item-toggle', event => {
       const id = getItemIdFromElement(event.currentTarget);
-      store.findAndToggleChecked(id);
-      render();
+      const item = store.findById(id);
+      api.updateItem(id, { checked: !item.checked }, () => {
+        store.findAndUpdate(id, { checked: !item.checked });
+        render();
+      });
     });
   }
   
@@ -102,8 +106,10 @@ const shoppingList = (function(){
       event.preventDefault();
       const id = getItemIdFromElement(event.currentTarget);
       const itemName = $(event.currentTarget).find('.shopping-item').val();
-      store.findAndUpdateName(id, itemName);
-      render();
+      api.updateItem(id, { name: newName }, () => {
+        store.findAndUpdate(id, { name: newName });
+        render();
+      });
     });
   }
   
